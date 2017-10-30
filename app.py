0cb93126e336856daffba3a98e83060eb70b5ea0
ZhaoGuan <ZhaoGuan@users.noreply.github.com>
2017-10-31 00:08:37 +0800

    fix bug

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 # __author__ = 'Gz'
 import os
-
-os.system('locust -f locustfile.py --host=http://api.kikakeyboard.com')
+while True:
+    choose = input("Tell me how you're going to start？ Master or Slave")
+    if choose == "Master":
+        os.system('locust -f locustfile.py --host=http://api.kikakeyboard.com')
+        break
+    elif choose == 'Slave':
+        master = input('Tall me your Master:')
+        os.system('locust -f locustfile.py --slave --master-host=%s' % master)
+        break
+    else:
+        print('You input wrong!! Try again!')
