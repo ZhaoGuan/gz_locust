fbefb0a8819898d7acce9c11af591c28efab96c5
ZhaoGuan <ZhaoGuan@users.noreply.github.com>
2017-10-30 17:31:32 +0800

    this is begin

---
+++ check.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# __author__ = 'Gz'
+
+a = '/s?wd=locust&rsv_spt=1&rsv_iqid=0x83390cb000021f87&issp=1&f=8&rsv_bp=1&rsv_idx=2&ie=utf-8&rqlang=cn&tn=baiduhome_pg&rsv_enter=1&oq=12%2526lt%253B12%2526lt%253B1&inputT=1537&rsv_t=7dedwQoSVUB9CjgOEMorpwCO83bZ3z1WoE1LPbEf5SHfY4hO9T9ubNUzuSRFfBu2g6RU&rsv_pq=cc32f9460002b63f&rsv_sug3=13&rsv_sug1=8&rsv_sug7=100&bs=1231231'
---
+++ locustfile.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# __author__ = 'Gz'
+from locust import HttpLocust, TaskSet, task
+
+
+class popup_test(TaskSet):
+    def on_start(self):
+        """ on_start is called when a Locust start before any task is scheduled """
+        self.popup()
+
+    @task(5)
+    def popup(self):
+        # self.client.header()
+        header = {
+            'User-Agent': 'com.qisiemoji.inputmethod/2021 (175b40b82dac4a5e95e3976cebccd7ac/78472ddd7528bcacc15725a16aeec190) Country/AU Language/en System/android Version/23 Screen/480',
+            'Accept-Charset': 'UTF-8', 'Kika-Install-Time': '1503996692777', 'Accept-Encoding': 'gzip',
+            'X-Model': 'D6603', 'If-Modified-Since': 'Mon, 04 Sep 2017 07:08:32 GMT', 'Accept-Language': 'en_AU',
+            'Host': 'api-dev.kikakeyboard.com', 'Connection': 'Keep-Alive'}
+        # pop = self.client.get(
+        #     'https://api.kikakeyboard.com/v1/stickers2/popup?tag=lol&kb_lang=en_AU&sign=87d6cf9df3294d23b6ac7d85b28d4491',
+        #     headers=header, catch_response=True)
+        pop = self.client.get(
+            'http://52.43.155.219:9090/backend-content-sending/popup?tag=lol&kb_lang=en_AU&sign=87d6cf9df3294d23b6ac7d85b28d4491',
+            headers=header, catch_response=True)
+        with pop as response:
+            try:
+                if response.json()['errorMsg'] != 'ok':
+                    response.failure('wrong!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+            except:
+                pass
+        print(pop.json())
+
+
+class MyLocust(HttpLocust):
+    task_set = popup_test
+    min_wait = 5000
+    max_wait = 9000
