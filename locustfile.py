01c79db36e42210192a193e938ef1d6aefe0e486
ZhaoGuan <ZhaoGuan@users.noreply.github.com>
2017-10-31 16:10:28 +0800

    change test to online

--- locustfile.py
+++ locustfile.py
@@ -11,24 +11,29 @@ class popup_test(TaskSet):
     @task(5)
     def popup(self):
         # self.client.header()
-        header = {
+        header_online = {
+            'User-Agent': 'com.qisiemoji.inputmethod/2021 (175b40b82dac4a5e95e3976cebccd7ac/78472ddd7528bcacc15725a16aeec190) Country/AU Language/en System/android Version/23 Screen/480',
+            'Accept-Charset': 'UTF-8', 'Kika-Install-Time': '1503996692777', 'Accept-Encoding': 'gzip',
+            'X-Model': 'D6603', 'If-Modified-Since': 'Mon, 04 Sep 2017 07:08:32 GMT', 'Accept-Language': 'en_AU',
+            'Host': 'api.kikakeyboard.com', 'Connection': 'Keep-Alive'}
+        header_test = {
             'User-Agent': 'com.qisiemoji.inputmethod/2021 (175b40b82dac4a5e95e3976cebccd7ac/78472ddd7528bcacc15725a16aeec190) Country/AU Language/en System/android Version/23 Screen/480',
             'Accept-Charset': 'UTF-8', 'Kika-Install-Time': '1503996692777', 'Accept-Encoding': 'gzip',
             'X-Model': 'D6603', 'If-Modified-Since': 'Mon, 04 Sep 2017 07:08:32 GMT', 'Accept-Language': 'en_AU',
             'Host': 'dev-api.kikakeyboard.com', 'Connection': 'Keep-Alive'}
         # pop = self.client.get(
         #     'https://api.kikakeyboard.com/v1/stickers2/popup?tag=lol&kb_lang=en_AU&sign=87d6cf9df3294d23b6ac7d85b28d4491',
-        #     headers=header, catch_response=True)
+        #     headers=header_online, catch_response=True)
         pop = self.client.get(
             'http://52.43.155.219:9090/backend-content-sending/popup?tag=lol&kb_lang=en_AU&sign=87d6cf9df3294d23b6ac7d85b28d4491',
-            headers=header, catch_response=True)
+            headers=header_test, catch_response=True)
         with pop as response:
             try:
                 if response.json()['errorMsg'] != 'ok':
                     response.failure('wrong!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
             except:
                 pass
-        print(pop.json())
+        # print(pop.json())
 
 
 class MyLocust(HttpLocust):
