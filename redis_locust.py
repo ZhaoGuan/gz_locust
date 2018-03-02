2805329ba875cc9d515429b31dbfaf84532b357a
ZhaoGuan <ZhaoGuan@users.noreply.github.com>
2018-03-03 02:23:09 +0800

    save

--- redis_locust.py
+++ redis_locust.py
@@ -5,49 +5,32 @@ import redis
 from locust import Locust, TaskSet, events, task
 
 
-# class RedisClient(redis.Redis):
-#     def __getattr__(self, host, port=6379, db=0):
-#         func = redis.Redis.__getattr__(self, host, port, db)
-#
-#         def wrapper(*args, **kwargs):
-#             start_time = time.time()
-#             try:
-#                 result = func(*args, **kwargs)
-#                 total_time = int((time.time() - start_time) * 1000)
-#                 events.request_success.fire(request_type="redis", host=host, port=port, db=db, response_time=total_time)
-#             except redis.exceptions as e:
-#                 total_time = int((time.time() - start_time) * 1000)
-#                 events.request_failure.fire(request_type="redis", host=host, port=port, db=db, response_time=total_time,
-#                                             exception=e)
-#             else:
-#                 total_time = int((time.time() - start_time) * 1000)
-#                 events.request_success.fire(request_type="redis", host=host, port=port, db=db, response_time=total_time)
-#
-#         return wrapper
-class RedisClient(redis):
-    def __getattr__(self):
-        func = redis
+class RedisClient(redis.Redis):
+    def __getattr__(self, host, port=6379, db=0):
+        func = redis.Redis.__getattr__(self, host, port, db)
 
-        def wrapper():
+        def wrapper(*args, **kwargs):
             start_time = time.time()
             try:
-                result = func
-                total_time = int((time.time() - start_time) * 1000)
-                events.request_success.fire(request_type="redis", response_time=total_time)
+                result = func(*args, **kwargs)
             except redis.exceptions as e:
                 total_time = int((time.time() - start_time) * 1000)
-                events.request_failure.fire(request_type="redis", response_time=total_time,
+                events.request_failure.fire(request_type="redis", host=host, port=port, db=db, response_time=total_time,
                                             exception=e)
             else:
                 total_time = int((time.time() - start_time) * 1000)
-                events.request_success.fire(request_type="redis", response_time=total_time)
+                events.request_success.fire(request_type="redis", host=host, port=port, db=db, response_time=total_time,
+                                            response_length=0)
+                print('!!!!!!!!!!!!!!!!!!!!!!!!!')
 
         return wrapper
+
+
 
 class RedisLocust(Locust):
     def __init__(self, *args, **kwargs):
         super(RedisLocust, self).__init__()
-        self.client = RedisClient()
+        self.client = RedisClient(self.host, port=6379, db=0)
 
 
 class Redis_test(RedisLocust):
@@ -58,10 +41,10 @@ class Redis_test(RedisLocust):
     class task_set(TaskSet):
         @task(10)
         def duid_data(self):
-            data = self.client.Redis('kika-data-blau-redis0.intranet.com').lrange('USER_RECENT_NUM_b4b7d7cc9454464b938177d412874ce9', 0, 10)
+            data = self.client.lrange('USER_RECENT_NUM_b4b7d7cc9454464b938177d412874ce9', 0, 10)
             print(data)
 
         @task(0)
         def duid_tag_data(self):
-            data = self.client.Redis('kika-data-blau-redis0.intranet.com').zrange('USER_RECENT_TAG_NUM_na_a1be32a10c414065a20e7b755018150c', 0, 10)
+            data = self.client.zrange('USER_RECENT_TAG_NUM_na_a1be32a10c414065a20e7b755018150c', 0, 10)
             print(data)
