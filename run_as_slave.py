6bbe536677095924db91f84e24495e8686c044f5
ZhaoGuan <ZhaoGuan@users.noreply.github.com>
2018-04-16 14:22:29 +0800

    save

--- run_as_slave.py
+++ run_as_slave.py
@@ -4,5 +4,6 @@ import os
 
 host = input('输入master的host:')
 run_no = input('请输入要启动的slave数量:')
+file = input('请处输入执行文件:')
 for i in range(int(run_no)):
-    os.system('nohup locust -f locustfile.py --slave --master-host=%s &' % host)
+    os.system('nohup locust -f %s --slave --master-host=%s &' % (host, file))
