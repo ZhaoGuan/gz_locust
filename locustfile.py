defccb5478175aad586538678cabc9df84bb1120
ZhaoGuan <ZhaoGuan@users.noreply.github.com>
2018-05-24 14:23:20 +0800

    save

--- locustfile.py
+++ locustfile.py
@@ -17,7 +17,7 @@ all_data = a.url_keys_data()
 
 
 class popup_test(TaskSet):
-    @task(0)
+    @task(10)
     def popup(self):
         # self.client.header()
         lang = ['en_AU', 'pt_BR', 'es_AR', 'in_ID']
@@ -29,17 +29,17 @@ class popup_test(TaskSet):
             'Accept-Charset': 'UTF-8', 'Kika-Install-Time': '1503996692777', 'Accept-Encoding': 'gzip',
             'X-Model': 'D6603', 'If-Modified-Since': 'Mon, 04 Sep 2017 07:08:32 GMT', 'Accept-Language': kb_lang,
             'Host': 'api.kikakeyboard.com', 'Connection': 'Keep-Alive'}
-        # header_test = {
-        #     'User-Agent': 'com.qisiemoji.inputmethod/2021 (175b40b82dac4a5e95e3976cebccd7ac/78472ddd7528bcacc15725a16aeec190) Country/AU Language/en System/android Version/23 Screen/480',
-        #     'Accept-Charset': 'UTF-8', 'Kika-Install-Time': '1503996692777', 'Accept-Encoding': 'gzip',
-        #     'X-Model': 'D6603', 'If-Modified-Since': 'Mon, 04 Sep 2017 07:08:32 GMT', 'Accept-Language': 'en_AU',
-        #     'Host': 'dev-api.kikakeyboard.com', 'Connection': 'Keep-Alive'}
-        pop = self.client.get(
-            'https://api.kikakeyboard.com/v1/stickers2/popup?tag=lol&kb_lang=en_AU&sign=87d6cf9df3294d23b6ac7d85b28d4491',
-            headers=header_online, catch_response=True)
+        header_test = {
+            'User-Agent': 'com.qisiemoji.inputmethod/2021 (175b40b82dac4a5e95e3976cebccd7ac/78472ddd7528bcacc15725a16aeec190) Country/' + country + ' Language/' + lang + ' System/android Version/23 Screen/480',
+            'Accept-Charset': 'UTF-8', 'Kika-Install-Time': '1503996692777', 'Accept-Encoding': 'gzip',
+            'X-Model': 'D6603', 'If-Modified-Since': 'Mon, 04 Sep 2017 07:08:32 GMT', 'Accept-Language': kb_lang,
+            'Host': 'dev-api.kikakeyboard.com', 'Connection': 'Keep-Alive'}
         # pop = self.client.get(
-        #     'http://52.43.155.219:9090/backend-content-sending/popup?tag=lol&kb_lang=en_AU&sign=87d6cf9df3294d23b6ac7d85b28d4491',
-        #     headers=header_test, catch_response=True)
+        #     'https://api.kikakeyboard.com/v1/stickers2/popup?tag=lol&kb_lang=en_AU&sign=87d6cf9df3294d23b6ac7d85b28d4491',
+        #     headers=header_online, catch_response=True)
+        pop = self.client.get(
+            'http://172.31.23.134:9090/backend-content-sending/popup?tag=ok&kb_lang=' + kb_lang + '&sign=87d6cf9df3294d23b6ac7d85b28d4491',
+            headers=header_test, catch_response=True)
         with pop as response:
             try:
                 if response.json()['errorMsg'] != 'ok':
@@ -1147,5 +1147,5 @@ class MyLocust(HttpLocust):
     # host = 'api.kikakeyboard.com'
     # host = 'blau.kika-backend.com'
     # host = 'kika-data-blau-web0.intranet.com'
-    host = 'api.giphy.com'
+    # host = 'api.giphy.com'
     # host = 'https://www.baidu.com/'
