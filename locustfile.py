d640d2d368edde10d1f8ecdbf118fbea02900d7e
ZhaoGuan <ZhaoGuan@users.noreply.github.com>
2018-07-02 12:14:31 +0800

    save

--- locustfile.py
+++ locustfile.py
@@ -1308,7 +1308,7 @@ class popup_test(TaskSet):
 
     @task(10)
     def OpTag(self):
-        url = 'http://34.214.222.244:9090/backend-content-sending/popup?tag=good&kb_lang=en_US&sign=a15d5bcd3a310c773f47955a636022cc&type=0'
+        url = 'http://172.31.23.134:9090/backend-content-sending/popup?tag=good&kb_lang=en_US&sign=a15d5bcd3a310c773f47955a636022cc&type=0'
         header = {'X-Model': 'D6603', 'Connection': 'Keep-Alive',
                   'User-Agent': 'com.qisiemoji.inputmethod/2541 (c4b18139641abdd81512120cba3ba4fd/78472ddd7528bcacc15725a16aeec190) Country/US Language/en System/android Version/23 Screen/480',
                   'Accept-Language': 'en_US', 'Accept-Encoding': 'gzip', 'Kika-Install-Time': '1505198889124'}
