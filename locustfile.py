d198c7004d7c91985d3b1e3884c8cd37118755f2
ZhaoGuan <guanzhaogz@gmail.com>
2018-12-12 23:51:44 +0800

    save

--- locustfile.py
+++ locustfile.py
@@ -178,7 +178,7 @@ class WuRen(TaskSet):
         print(request_header)
         print(response.json())
 
-    @task(10)
+    @task(0)
     def search_hot(self):
         user_data = random.choice(user_datas)
         duid = user_data['duid']
@@ -210,7 +210,7 @@ class WuRen(TaskSet):
                      'path': './case/search/search_hot.yml'}
         true_requets(self.client, case_data)
 
-    @task(0)
+    @task(10)
     def search_fall(self):
         user_data = random.choice(user_datas)
         duid = user_data['duid']
